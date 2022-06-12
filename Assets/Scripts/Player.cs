e9c9c58451dca4c3a0b184807a987591cbe66dd2
reoreo <57691895+reoreo-zyt@users.noreply.github.com>
2022-06-12 16:53:51 +0800

    动画
    通过添加精灵图进行切换

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // 动画
+    private SpriteRenderer spriteRenderer;
+    // 通过设置为public将sprites图加载进去
+    public Sprite[] sprites;
+    private int spriteIndex;
     private Vector3 direction;
     // 重力
     public float gravity = -9.8f;
     // 方向移动距离
     public float strength = 5f;
 
-    public void Update() 
+    // 初始化时被调用
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        // 重复调用函数
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void Update() 
     {
         // 空格键或者鼠标单击
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
@@ -32,5 +49,17 @@ public class Player : MonoBehaviour
         // 间隔时间下降，更新鸟的位置
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if(spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
