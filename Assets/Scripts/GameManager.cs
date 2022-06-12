2b0928e987d07a6b9ef1b0066d42c91d9967eea9
reoreo <57691895+reoreo-zyt@users.noreply.github.com>
2022-06-12 22:32:47 +0800

    游戏结束以及得分

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+    }
+}
